a402c00f7733e917a1c98f1ddb4a5e3444617d1e
Zoe <zyoh.32@gmail.com>
2021-07-16 15:18:03 -0400

    init

---
+++ VRChatPNG/VRChatPNG.py
@@ -0,0 +1,188 @@
+from pathlib import Path
+from tqdm import tqdm
+from PIL import Image
+import requests
+import shutil
+import json
+import time
+import sys
+
+from VRChatThumbnail import VRChatThumbnail
+from options import Options
+
+
+class Downloader:
+	def __init__(self):
+		self.Session = requests.session()
+		self.Session.headers.update({'User-Agent': 'Requests/{}'.format(requests.__version__)})
+
+	def is_url(self, url: str):
+		assert isinstance(url, str)
+		return url.lower().startswith('http')
+
+	def download(self, url: str, destination: Path):
+		assert self.is_url(url)
+		assert isinstance(destination, Path)
+
+		# Ty to https://stackoverflow.com/a/10744565
+		response = self.Session.get(url, stream=True)
+		with open(destination, "wb") as handle:
+			try:
+				for data in tqdm(response.iter_content(chunk_size=8192)):
+					handle.write(data)
+			except NameError:
+				for data in response.iter_content(chunk_size=8192):
+					handle.write(data)
+
+	@classmethod
+	def qdownload(cls, url: str, destination: Path):
+		dler = cls()
+		dler.download(url, destination)
+
+
+class App:
+	def __init__(self, json_file_path: Path):
+		assert isinstance(json_file_path, Path)
+		self.json_file_path = json_file_path
+
+		# If error: delete these
+		self.floating_dir = None
+		self.floating_files = []
+
+	@staticmethod
+	def vrc_url_filename(file_url):
+		return '.'.join(file_url.split('/')[6:-1])
+
+	def delete_floating(self):
+		for file in self.floating_files:
+			assert isinstance(file, Path)
+			assert file.exists()
+
+			file.unlink()
+		
+		if self.floating_dir is not None:
+			assert isinstance(self.floating_dir, Path)
+			assert self.floating_dir.is_dir()
+
+			shutil.rmtree(self.floating_dir)
+
+	def run(self):
+		with open(self.json_file_path, 'r', encoding='utf-8') as f:
+			data = json.load(f)
+			assert type(data) in (list, dict)
+
+			if isinstance(data, list):
+				for c, avatar_data in enumerate(data):
+					self._run(avatar_data)
+					if c < len(data)-1:
+						time.sleep(1)
+						print()
+			elif isinstance(data, dict):
+				self._run(data)
+		
+	def _run(self, data: dict):
+		assert isinstance(data, dict)
+
+		if "favoriteId" in data:
+			del data["favoriteId"]
+		if "favoriteGroup" in data:
+			del data["favoriteGroup"]
+
+		print(f"Downloading: {data.get('id')}")
+
+		unique_name = f"{data.get('id')}.{data.get('version')}"
+
+		# Make folder for files
+		working_dir = self.json_file_path.parent / f".{unique_name}"
+		working_dir.mkdir()
+		self.floating_dir = working_dir
+
+		# Copy json to folder
+		with open(working_dir / (unique_name + ".json"), 'w', encoding='utf-8', errors='ignore') as f:
+			json.dump(data, f, indent=4, ensure_ascii=False)
+
+		# Download image
+		image_url = data.get('imageUrl')
+		image_path = working_dir / (self.vrc_url_filename(image_url) + ".png")
+		Downloader.qdownload(image_url, image_path)
+
+		# Get avatar asset files - No longer available
+		pass
+		
+		# Compress folder to zip
+		shutil.make_archive(working_dir.parent / unique_name, 'zip', working_dir)
+		self.floating_files.append(working_dir.parent / (unique_name + ".zip"))
+
+		# ---
+
+		# Get platforms	
+		platform_support = []
+		for p in ["standalonewindows", "android"]:
+			platform_support.append(
+				len(list(filter(lambda x: x['platform'] == p, data.get('unityPackages')))) > 0
+			)
+
+		if platform_support[0] and not platform_support[1]:
+			platform = VRChatThumbnail.Platform.PC
+		elif platform_support[1] and not platform_support[0]:
+			platform = VRChatThumbnail.Platform.Quest
+		else:
+			platform = VRChatThumbnail.Platform.Both
+
+		# Make thumbnail
+		VRChatThumbnail.make_thumbnail(
+			Image.open(str(image_path)),
+			platform=platform,
+			avatar_name=data.get('name'),
+			author_name=data.get('authorName'),
+			data_dir=Path(__file__).parent / "data"
+		).save(
+			str(working_dir / 'image.png')
+		)
+		self.floating_files.append(working_dir / 'image.png')
+
+		# ---
+
+		# Combine image with zip
+		with open(working_dir.parent / (unique_name + ".png"), 'wb') as f_out:
+			with open(working_dir / 'image.png', 'rb') as in_img:
+				f_out.write(in_img.read())
+			with open(working_dir.parent / (unique_name + ".zip"), 'rb') as in_zip:
+				f_out.write(in_zip.read())
+
+		# Delete extra files
+		shutil.rmtree(working_dir)
+		self.floating_dir = None
+		
+		(working_dir.parent / (unique_name + ".zip")).unlink()
+		self.floating_files = []
+
+
+def main():
+	options = Options(sys.argv)
+	HELP = """
+Options:
+    -h, --help      Display help message and exit.
+
+    -i <path>       Path to JSON file containing avatar data.
+"""
+
+	if options.get("-h", False) or options.get("--help", False):
+		pass
+	elif json_path := options.get("-i", True):
+		json_path = Path(json_path).resolve()
+
+		app = App(json_path)
+		try:
+			app.run()
+		except Exception as e:
+			app.delete_floating()
+			raise e
+
+		return
+	
+	print(HELP)
+
+
+if __name__ == "__main__":
+	main()
---
+++ VRChatPNG/VRChatThumbnail.py
@@ -0,0 +1,156 @@
+from PIL import Image, ImageDraw, ImageFont
+from pathlib import Path
+from enum import Enum
+import numpy as np
+import time
+import sys
+
+from options import Options
+
+
+class VRChatThumbnail:
+
+	class Platform(Enum):
+		Both = 0
+		PC = 1
+		Quest = 2
+
+	@staticmethod
+	def make_thumbnail(avatar_image: Image, platform, avatar_name: str, author_name: str, data_dir: Path) -> Image:
+		assert isinstance(platform, VRChatThumbnail.Platform)
+		assert isinstance(avatar_name, str)
+		assert isinstance(author_name, str)
+
+		if isinstance(platform, VRChatThumbnail.Platform):
+			platform = platform.value
+
+		text_overlay = Image.open(data_dir / 'text_overlay.png')
+		corner_mask = Image.open(data_dir / 'corner_mask.png')
+		platform_icon = Image.open(data_dir / 'vrc_platform.png')
+
+		if (data_dir / 'font.ttf').is_file():
+			font_path = data_dir / 'font.ttf'
+		elif (data_dir / 'font.otf').is_file():
+			font_path = data_dir / 'font.otf'
+
+		# --- Set up images ---
+		
+		# Base
+		avatar_image = avatar_image.convert('RGBA')
+		avatar_image = avatar_image.resize((1200, 900))
+
+		# Dark bg for text
+		if avatar_image.size != text_overlay.size:
+			# Resize to match
+			text_overlay.resize( avatar_image.size, Image.ANTIALIAS )
+		text_overlay = text_overlay.convert('RGBA')
+
+		# Cut corners to match VRChat UI
+		if avatar_image.size != corner_mask.size:
+			# Resize to match
+			corner_mask.resize( avatar_image.size, Image.ANTIALIAS )
+		corner_mask = corner_mask.convert('L')
+
+		# - Platform support icon -
+		platform_icon = platform_icon.convert('RGBA')
+		# platform_icon.putalpha(Image.new("L", platform_icon.size, 75))
+		platform_icon.putalpha(
+			Image.fromarray(np.asarray(platform_icon.split()[-1]) * 0.85).convert("L")
+		)
+		
+		# Width should be 512 on a background with a width of 1200
+		_icon_width = int( 320 / 1200 * avatar_image.size[0] )
+		_icon_height = int( platform_icon.size[1] / platform_icon.size[0] * _icon_width )
+		platform_icon = platform_icon.resize( (_icon_width, _icon_height), Image.ANTIALIAS )
+
+		# Crop icon set to the correct icon
+		platform_icon = platform_icon.crop((
+			0,
+			int( platform_icon.size[1]/3 * platform ),
+			platform_icon.size[0],
+			int( platform_icon.size[1]/3 * (platform+1) )
+		))
+
+		# --- Merge images ---
+		avatar_image.paste( text_overlay, (0,0), text_overlay )
+		
+		_icon_offset = int( 25/900 * avatar_image.size[1] )
+		avatar_image.paste( platform_icon, (_icon_offset,_icon_offset), platform_icon )
+		avatar_image.putalpha(corner_mask)
+
+		# --- Write text ---
+		_font_title = ImageFont.truetype( str(font_path) , int(avatar_image.size[0]*0.1) )
+		_text_offset_x_title = int( avatar_image.size[0] * 0.1 )
+		_text_offset_y_title = int( avatar_image.size[1] - avatar_image.size[1] * 0.3 )
+		ImageDraw.Draw(avatar_image).text(
+			( _text_offset_x_title, _text_offset_y_title ),
+			avatar_name,
+			(255,255,255),
+			font=_font_title
+		)
+
+		_font_small = ImageFont.truetype( str(font_path) , int(avatar_image.size[0]*0.065) )
+		_text_offset_x_small = int( avatar_image.size[0] * 0.2 )
+		_text_offset_y_small = int( avatar_image.size[1] - avatar_image.size[1] * 0.125 )
+		ImageDraw.Draw(avatar_image).text(
+			( _text_offset_x_small, _text_offset_y_small ),
+			author_name,
+			(255,255,0),
+			font=_font_small
+		)
+
+		return avatar_image
+
+
+def main():
+	options = Options(sys.argv)
+
+	HELP = """
+Options:
+    -h, --help                      Display help message and exit.
+
+    -i <path>                       Path to image.
+    -p <int>, --platform <int>      Supported platforms.
+        - 0 | PC & Quest
+        - 1 | PC
+        - 2 | Quest
+    -n <str>, --name <str>          Name of avatar.
+    -a <str>, --author <str>        Name of avatar author.
+    
+"""
+
+	if options.get("-h", False) or options.get("--help", False):
+		pass
+	elif (image_path := options.get("-i")) and \
+		(platform := options.get("-p") or options.get("--platform")) and \
+		(platform in ["0", "1", "2"]) and \
+		(avatar_name := options.get("-n") or options.get("--name")) and \
+		(author_name := options.get("-a") or options.get("--author")):
+
+		image_path = Path(image_path).resolve()
+		assert image_path.exists()
+
+		if out_path := options.get("-o"):
+			out_path = Path(out_path).resolve()
+			assert out_path.exists()
+		else:
+			out_path = image_path.parent / (f"{time.time()}." + image_path.stem + ".png")
+
+		platform = VRChatThumbnail.Platform(int(platform))
+
+		img = VRChatThumbnail.make_thumbnail(
+			avatar_image=Image.open(image_path),
+			platform=platform,
+			avatar_name=avatar_name,
+			author_name=author_name,
+			data_dir=Path(__file__).parent / "data"
+		)
+		img.save(out_path)
+
+		return
+	
+	print(HELP)
+
+
+if __name__ == "__main__":
+	main()
